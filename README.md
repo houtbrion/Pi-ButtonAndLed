9b608541b76e600016c5335f3000b8299c21e330
houtbrion <houtbrion@gmail.com>
2017-08-25 17:32:57 +0900

    buttonが動くように修正

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # Pi-ButtonAndLed
 read button status and control LED on GPIO.
+
+
+## 概要
+
+GPIOに繋がっているボタンの状態を読み取って，しばらく
+押し続けてられていたら，LEDを点灯して，ボタンに
+結び付けられている処理を実行する．
+
+
+## ボタンを押した場合の処理
+
+### 案1 snmp
+SNMP
+
+### 案2 他のホストにhttpで通知
+HTTPで通知
+
+
--- buttonAndLed.py
+++ buttonAndLed.py
@@ -1,21 +1,24 @@
 #!/usr/bin/python
+# -*- coding: utf-8 -*-
 
 from time import sleep
 import RPi.GPIO as GPIO
 
 # ピン番号
-# ボタン : GPIO 21  (ピン番号40)
-# LED    : GPIO 20  (ピン番号38)
-buttonPin = 21
-ledPin = 20
+# ボタン : GPIO 20  (ピン番号38)
+# LED    : GPIO 16  (ピン番号36)
+buttonPin = 20
+ledPin = 16
 
 # ボタンをチェックするループの待ち時間
-loopWait = 0.05
+loopWait = 0.1
+
 # ボタンをONと判定する閾値
-buttonThreshold=10
+buttonThreshold=5
 
 # ピンのセットアップ
 def setupPins():
+    GPIO.setwarnings(False)
     GPIO.setmode(GPIO.BCM)
     GPIO.setup(buttonPin, GPIO.IN)
     GPIO.setup(ledPin, GPIO.OUT)
@@ -23,10 +26,10 @@ def setupPins():
 # アクションの実行
 def doAction():
     GPIO.output(ledPin,GPIO.HIGH)
-    sleep(1)
+    sleep(2)
     GPIO.output(ledPin,GPIO.LOW)
 
-
+# メインループ
 if __name__ == "__main__":
     setupPins()
     buttonCounter=0
